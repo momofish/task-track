0f80754e8e1dbd34bea438066bb3669798fd5c8f
momofish <momofish@gmail.com>
2016-05-09 17:31:20 +0800

    -

--- app/components/QuickAdd.js
+++ app/components/QuickAdd.js
@@ -36,14 +36,6 @@ class QuickAdd extends React.Component {
         <div className='input-group'>
           <input type='text' className='form-control' placeholder={this.props.placeHolder} value={this.state.title} onChange={this.handleChange.bind(this)} />
           <span className='input-group-btn'>
-            <button type="button" className="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Action <span class="caret"></span></button>
-            <ul className="dropdown-menu dropdown-menu-right">
-              <li><a href="#">Action</a></li>
-              <li><a href="#">Another action</a></li>
-              <li><a href="#">Something else here</a></li>
-              <li role="separator" class="divider"></li>
-              <li><a href="#">Separated link</a></li>
-            </ul>
             <button className='btn btn-default' onClick={this.handleSubmit.bind(this)}><span className='glyphicon glyphicon-plus'></span></button>
           </span>
         </div>
