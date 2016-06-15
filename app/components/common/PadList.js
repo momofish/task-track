9c490508670ba9d5ab0b0444b0a07ceaf31d7fee
momofish <momofish@gmail.com>
2016-06-15 22:40:01 +0800

    pad view fix

--- app/components/common/PadList.js
+++ app/components/common/PadList.js
@@ -18,33 +18,35 @@ class PadList extends Component {
     let groups = this.props.data;
 
     return (
-      <div className='flex flex-horiziontal flex-hscroll'>
-        {groups.map((group, i) => (
-          <div className='pad-list' key={`${i}`}>
-            <div className='pad-wrap'>
-              <div className='pad-header'>{group.header.label}</div>
-              <ul className={`pad-body`}>
-                {group.body.map((item, j) => (
-                  <li className='pad-item' key={`${j}`}
-                    onClick={this.handleSelect.bind(this, item.data) }>
-                    <div className={classnames('pad-title', { completed: item.completed }) }>
-                      {item.label}
-                    </div>
-                    <div className='pad-tags'>
-                      {item.tags.filter(tag => tag.label || tag.icon).map((tag, k) => (
-                        <span key={`${k}`} className={`tag tag-${tag.style}`}
-                          onClick={this.handleClickTag.bind(this, item.data, tag) }>
-                          {tag.icon && <i className={`${'glyphicon glyphicon-' + tag.icon}`} />}
-                          {tag.label}
-                        </span>
-                      )) }
-                    </div>
-                  </li>
-                )) }
-              </ul>
+      <div className='flex flex-hscroll'>
+        <div className='scroll-container'>
+          {groups.map((group, i) => (
+            <div className='pad-list' key={`${i}`}>
+              <div className='pad-wrap'>
+                <div className='pad-header'>{group.header.label}</div>
+                <ul className={`pad-body`}>
+                  {group.body.map((item, j) => (
+                    <li className='pad-item' key={`${j}`}
+                      onClick={this.handleSelect.bind(this, item.data) }>
+                      <div className={classnames('pad-title', { completed: item.completed }) }>
+                        {item.label}
+                      </div>
+                      <div className='pad-tags'>
+                        {item.tags.filter(tag => tag.label || tag.icon).map((tag, k) => (
+                          <span key={`${k}`} className={`tag tag-${tag.style}`}
+                            onClick={this.handleClickTag.bind(this, item.data, tag) }>
+                            {tag.icon && <i className={`${'glyphicon glyphicon-' + tag.icon}`} />}
+                            {tag.label}
+                          </span>
+                        )) }
+                      </div>
+                    </li>
+                  )) }
+                </ul>
+              </div>
             </div>
-          </div>
-        )) }
+          )) }
+        </div>
       </div>
     );
   }
