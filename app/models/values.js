f92eedcec62843292e900698bedf8d8f982d5ab2
momofish <momofish@gmail.com>
2016-12-27 17:41:11 +0800

    任务分类名称更改

--- app/models/values.js
+++ app/models/values.js
@@ -1,8 +1,8 @@
 import moment from 'moment';
 
 export const taskTreat = {
-  0: { name: '未分配' },
-  10: { name: '今天做', style: 'success' },
+  0: { name: '新任务' },
+  10: { name: '马上做', style: 'success' },
   20: { name: '下一步做', collapsed: true, style: 'info' },
   30: { name: '以后再做', collapsed: true, style: 'warning' },
 }
