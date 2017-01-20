a4096cf7a50bb2a30f679f556d41484d40c4bc9d
momofish <momofish@gmail.com>
2017-01-20 14:49:14 +0800

    -

--- app/components/know/QuestionEditor.js
+++ app/components/know/QuestionEditor.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 
 export default class QuestionEditor extends Component {
+  componentDidMount() {
+    editormd('editormd');
+  }
+
   render() {
     return (
-      <div>
-        test
+      <div className='container-fluid flex flex-verticle'>
+        <div className='page-header'>
+          <h2>
+            <i className='glyphicon glyphicon-tasks' /> 提问
+          </h2>
+        </div>
+        <div id='editormd'>
+          <textarea style={{ display: 'none' }}></textarea>
+        </div>
       </div>
     );
   }
