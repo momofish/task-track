39cf7a1b35eabc6a4ee87dba76944e054959088d
李强 <liqiang@liqiangdeMacBook-Air.local>
2017-03-04 10:06:59 +0800

    publish

--- publish.sh
+++ publish.sh
@@ -1,4 +1,21 @@
-git checkout master && git pull && git push && git checkout develop
-REMOTE_SH='export NODE_ENV=production && pm2 stop all && cd task-track && git pull && cnpm i && pm2 start all'
-ssh working@10.201.76.114 $REMOTE_SH
-ssh nginx@10.201.78.208 $REMOTE_SH
+# push to git
+git checkout master && git pull && git push
+
+# build
+export NODE_ENV=production
+gulp build
+
+# publish
+REMOTE_SH='cd task-track && git pull && cnpm i && pm2 restart all'
+
+REMOTE=working@10.201.76.114
+ssh $REMOTE $REMOTE_SH
+scp public/js/*.js $REMOTE:~/task-track/public/js/
+scp public/css/*.css $REMOTE:~/task-track/public/css/
+
+REMOTE=nginx@10.201.78.208
+ssh $REMOTE $REMOTE_SH
+scp public/js/*.js $REMOTE:~/task-track/public/js/
+scp public/css/*.css $REMOTE:~/task-track/public/css/
+
+git checkout develop
