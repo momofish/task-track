dce4ac41fbda1e5663045be9657652d418f217b1
李强 <liqiang@bingosoft.net>
2017-03-24 13:19:43 +0800

    loading

--- app/components/common/Button.js
+++ app/components/common/Button.js
@@ -4,9 +4,9 @@ import { Icon } from '.';
 
 class Button extends Component {
   render() {
-    let {type = 'button', className, icon, text, onClick, children} = this.props;
+    let {type = 'button', className, icon, text, disabled, onClick, children} = this.props;
     return (
-      <button onClick={onClick} type={type} className={classnames('btn btn-default', className)}>
+      <button onClick={onClick} type={type} disabled={disabled} className={classnames('btn btn-default', className)}>
         {icon && <Icon icon={icon} />}
         {text}
         {children}
